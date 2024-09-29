5b7c5b82a62fd0578f6721b15746c11c1785ec3a
dinesh920 <pmdileepyadav1506@gmail.com>
2024-09-29 10:22:04 +0530

    commit

--- main.tf
+++ main.tf
@@ -8,72 +8,72 @@ data "aws_availability_zones" "available" {
 }
 
 # Create a VPC for ECS
-resource "aws_vpc" "medusa_vpc_2025" {
+resource "aws_vpc" "medusa_vpc_2026" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    Name = "medusa-vpc-2025"
+    Name = "medusa-vpc-2026"
   }
 }
 
 # Create public subnets for the ECS tasks
-resource "aws_subnet" "public_subnet_1_2025" {
-  vpc_id                  = aws_vpc.medusa_vpc_2025.id
+resource "aws_subnet" "public_subnet_1_2026" {
+  vpc_id                  = aws_vpc.medusa_vpc_2026.id
   cidr_block              = var.public_subnet_1_cidr
   availability_zone       = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = true
   tags = {
-    Name = "medusa-public-subnet-1-2025"
+    Name = "medusa-public-subnet-1-2026"
   }
 }
 
-resource "aws_subnet" "public_subnet_2_2025" {
-  vpc_id                  = aws_vpc.medusa_vpc_2025.id
+resource "aws_subnet" "public_subnet_2_2026" {
+  vpc_id                  = aws_vpc.medusa_vpc_2026.id
   cidr_block              = var.public_subnet_2_cidr
   availability_zone       = data.aws_availability_zones.available.names[1]
   map_public_ip_on_launch = true
   tags = {
-    Name = "medusa-public-subnet-2-2025"
+    Name = "medusa-public-subnet-2-2026"
   }
 }
 
 # Create an Internet Gateway to allow access to the internet
-resource "aws_internet_gateway" "medusa_igw_2025" {
-  vpc_id = aws_vpc.medusa_vpc_2025.id
+resource "aws_internet_gateway" "medusa_igw_2026" {
+  vpc_id = aws_vpc.medusa_vpc_2026.id
   tags = {
-    Name = "medusa-igw-2025"
+    Name = "medusa-igw-2026"
   }
 }
 
 # Create a Route Table for public subnets
-resource "aws_route_table" "public_route_table_2025" {
-  vpc_id = aws_vpc.medusa_vpc_2025.id
+resource "aws_route_table" "public_route_table_2026" {
+  vpc_id = aws_vpc.medusa_vpc_2026.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.medusa_igw_2025.id
+    gateway_id = aws_internet_gateway.medusa_igw_2026.id
   }
   tags = {
-    Name = "medusa-public-route-table-2025"
+    Name = "medusa-public-route-table-2026"
   }
 }
 
 # Associate the Route Table with the Subnets
-resource "aws_route_table_association" "public_subnet_1_association_2025" {
-  subnet_id      = aws_subnet.public_subnet_1_2025.id
-  route_table_id = aws_route_table.public_route_table_2025.id
+resource "aws_route_table_association" "public_subnet_1_association_2026" {
+  subnet_id      = aws_subnet.public_subnet_1_2026.id
+  route_table_id = aws_route_table.public_route_table_2026.id
 }
 
-resource "aws_route_table_association" "public_subnet_2_association_2025" {
-  subnet_id      = aws_subnet.public_subnet_2_2025.id
-  route_table_id = aws_route_table.public_route_table_2025.id
+resource "aws_route_table_association" "public_subnet_2_association_2026" {
+  subnet_id      = aws_subnet.public_subnet_2_2026.id
+  route_table_id = aws_route_table.public_route_table_2026.id
 }
 
 # Security Group to allow HTTP/HTTPS access
-resource "aws_security_group" "medusa_sg_2025" {
-  name        = "medusa-sg-2025"
+resource "aws_security_group" "medusa_sg_2026" {
+  name        = "medusa-sg-2026"
   description = "Allow HTTP and HTTPS traffic"
-  vpc_id      = aws_vpc.medusa_vpc_2025.id
+  vpc_id      = aws_vpc.medusa_vpc_2026.id
 
   ingress {
     description = "Allow HTTP"
@@ -100,13 +100,13 @@ resource "aws_security_group" "medusa_sg_2025" {
 }
 
 # ECS Cluster to run the Medusa app
-resource "aws_ecs_cluster" "medusa_cluster_2025" {
-  name = "medusa-cluster-2025"
+resource "aws_ecs_cluster" "medusa_cluster_2026" {
+  name = "medusa-cluster-2026"
 }
 
 # Create IAM Role for ECS Task Execution
-resource "aws_iam_role" "ecs_task_execution_role_2025" {
-  name = "medusa-ecs-task-execution-role-2025"
+resource "aws_iam_role" "ecs_task_execution_role_2026" {
+  name = "medusa-ecs-task-execution-role-2026"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
@@ -120,23 +120,23 @@ resource "aws_iam_role" "ecs_task_execution_role_2025" {
   })
 
   tags = {
-    Name = "ecs_task_execution_role_2025"
+    Name = "ecs_task_execution_role_2026"
   }
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_2025" {
-  role       = aws_iam_role.ecs_task_execution_role_2025.name
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_2026" {
+  role       = aws_iam_role.ecs_task_execution_role_2026.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 # ECS Task Definition to run Medusa container
-resource "aws_ecs_task_definition" "medusa_task_definition_2025" {
-  family                   = "medusa-task-definition-2025"
+resource "aws_ecs_task_definition" "medusa_task_definition_2026" {
+  family                   = "medusa-task-definition-2026"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role_2025.arn
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role_2026.arn
   container_definitions    = <<DEFINITION
   [
     {
@@ -157,69 +157,69 @@ resource "aws_ecs_task_definition" "medusa_task_definition_2025" {
 }
 
 # ECS Service to run Medusa container
-resource "aws_ecs_service" "medusa_service_2025" {
-  name            = "medusa-service-2025"
-  cluster         = aws_ecs_cluster.medusa_cluster_2025.id
-  task_definition = aws_ecs_task_definition.medusa_task_definition_2025.arn
+resource "aws_ecs_service" "medusa_service_2026" {
+  name            = "medusa-service-2026"
+  cluster         = aws_ecs_cluster.medusa_cluster_2026.id
+  task_definition = aws_ecs_task_definition.medusa_task_definition_2026.arn
   desired_count   = 1
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets         = [aws_subnet.public_subnet_1_2025.id, aws_subnet.public_subnet_2_2025.id]
-    security_groups = [aws_security_group.medusa_sg_2025.id]
+    subnets         = [aws_subnet.public_subnet_1_2026.id, aws_subnet.public_subnet_2_2026.id]
+    security_groups = [aws_security_group.medusa_sg_2026.id]
   }
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.medusa_tg_2025.arn
+    target_group_arn = aws_lb_target_group.medusa_tg_2026.arn
     container_name   = "medusa"
     container_port   = 80
   }
 }
 
 # Create a Load Balancer to distribute traffic
-resource "aws_lb" "medusa_lb_2025" {
-  name               = "medusa-lb-2025"
+resource "aws_lb" "medusa_lb_2026" {
+  name               = "medusa-lb-2026"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.medusa_sg_2025.id]
-  subnets            = [aws_subnet.public_subnet_1_2025.id, aws_subnet.public_subnet_2_2025.id]
+  security_groups    = [aws_security_group.medusa_sg_2026.id]
+  subnets            = [aws_subnet.public_subnet_1_2026.id, aws_subnet.public_subnet_2_2026.id]
 
   enable_deletion_protection = false
 }
 
 # Create a Target Group for the Load Balancer
-resource "aws_lb_target_group" "medusa_tg_2025" {
-  name        = "medusa-tg-2025"
+resource "aws_lb_target_group" "medusa_tg_2026" {
+  name        = "medusa-tg-2026"
   port        = 80
   protocol    = "HTTP"
-  vpc_id      = aws_vpc.medusa_vpc_2025.id
+  vpc_id      = aws_vpc.medusa_vpc_2026.id
   target_type = "ip"
 }
 
 # Create a Listener for the Load Balancer
-resource "aws_lb_listener" "medusa_listener_2025" {
-  load_balancer_arn = aws_lb.medusa_lb_2025.arn
+resource "aws_lb_listener" "medusa_listener_2026" {
+  load_balancer_arn = aws_lb.medusa_lb_2026.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.medusa_tg_2025.arn
+    target_group_arn = aws_lb_target_group.medusa_tg_2026.arn
   }
 }
 
 # Outputs
 output "ecs_cluster_name" {
   description = "The name of the ECS cluster"
-  value       = aws_ecs_cluster.medusa_cluster_2025.name
+  value       = aws_ecs_cluster.medusa_cluster_2026.name
 }
 
 output "load_balancer_dns" {
   description = "DNS name of the load balancer"
-  value       = aws_lb.medusa_lb_2025.dns_name
+  value       = aws_lb.medusa_lb_2026.dns_name
 }
 
 output "service_name" {
   description = "The name of the ECS service"
-  value       = aws_ecs_service.medusa_service_2025.name
+  value       = aws_ecs_service.medusa_service_2026.name
 }
